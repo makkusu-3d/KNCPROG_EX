91992327c9338813f695433379a01e1ccfdc939f
makkusu-3d <maxdesign1990@gmail.com>
2018-08-26 12:19:49 +0100

    Update E1.c

--- CH4/Exercises/E1.c
+++ CH4/Exercises/E1.c
@@ -8,14 +8,17 @@ int i,j,k;
 
 int main(void)
 {
+    // (a)
     i = 5; j = 3;
     printf("%d %d\n", i / j, i % j);
     // Output: 1 2
-
+    
+    // (b)
     i = 2; j = 3;
     printf("%d\n", (i + 10) % j);
     // Output: 0
-
+    
+    // (c)
     i = 7; j = 8; k = 9;
     printf("%d\n", (i + 10) % k / j);
     // Output: 1
