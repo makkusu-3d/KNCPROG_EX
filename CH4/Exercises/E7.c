3db6c31688914082167c05041ade53a1cb79ceb0
makkusu-3d <maxdesign1990@gmail.com>
2018-08-26 16:01:51 +0100

    Create E7.c

---
+++ CH4/Exercises/E7.c
@@ -0,0 +1,50 @@
+/* The algorithm for computing the UPC check digit ends with
+ * the following steps:
+ *
+ * Subtract 1 from the total.
+ * Compute the remainder when the adjusted total is divided by 10.
+ * Subtract the remainder from 10
+ * 
+ * It's tempting to try to simplify the algorithm by using these
+ * steps instead
+ *
+ * Compute the remainder when the total is divided by 10.
+ * Subtract the remainder from 10.
+ *
+ * Why doesn't this technique work?
+ *
+/* Computes a Universal Product Code check digit */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5,
+        first_sum, second_sum, total;
+
+    printf("Enter the first (single) digit: ");
+    scanf("%1d", &d);
+    printf("Enter first group of five digits: ");
+    scanf("%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5);
+    printf("Enter second group of five digits: ");
+    scanf("%1d%1d%1d%1d%1d", &j1, &j2, &j3, &j4, &j5);
+    first_sum = d + i2 + i4 + j1 + j3 + j5;
+    second_sum = i1 + i3 + i5 + j2 + j4;
+    total = 3 * first_sum + second_sum;
+
+    printf("Check digit: %d\n", 9 - ((total - 1) % 10));
+
+    // Answer:
+    
+    // This technique doesn't work due to the difference in
+    // the order of operations for both expressions
+    // You cant bring the 1 over to the 9 from the first algorithm
+    // and have equivalent expressions due to the associative
+    // property with the modulus being the second operation
+    // It may work for some total values but for a majority it
+    // wont
+
+    printf("Method 2 - Check digit: %d", 10 - (total % 10));
+
+    return 0;
+}
