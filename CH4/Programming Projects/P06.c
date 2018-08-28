6558f2c539759d009863a811cbb2e97c36c7a6d5
makkusu-3d <maxdesign1990@gmail.com>
2018-08-28 11:03:56 +0100

    Create P06.c

---
+++ CH4/Programming Projects/P06.c
@@ -0,0 +1,29 @@
+/* Rewrite the upc.c program of Section 4.1 so that the user enters 11
+ * digits at one time, instead of entering one digit, then five digits,
+ * and then another five digits.
+ *
+ * 869148426000
+ */
+
+/* Computes a Universal Product Code check digit */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12,
+        first_sum, second_sum, total;
+
+    printf("Enter the first 12 digits of a EAN: ");
+
+    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",
+        &i1, &i2, &i3, &i4, &i5, &i6, &i7, &i8, &i9, &i10, &i11, &i12);
+
+    first_sum = i2 + i4 + i6 + i8 + i10 + i12;
+    second_sum = i1 + i3 + i5 + i7 + i9 + i11;
+    total = 3 * first_sum + second_sum;
+
+    printf("Check digit: %d\n", 9 - ((total - 1) % 10));
+
+    return 0;
+}
