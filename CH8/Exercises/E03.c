61b0fa08cbd00a59bb48fe7cf9f977d591e084fb
makkusu-3d <maxdesign1990@gmail.com>
2018-09-25 09:30:22 +0100

    Create E03.c

---
+++ CH8/Exercises/E03.c
@@ -0,0 +1,22 @@
+/* Write a declaration of an array named weekend
+ * containing seven bool values. Include an
+ * initializer that makes the first and last values
+ * true; all other values should be false.
+ *
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+
+// Declaration of weekend array
+bool weekend[7] = {[0] = 1, [6] = 1}; // 0 => False
+                                      // 1 => True
+int i;
+
+int main(void)
+{
+    for (i = 0; i < sizeof(weekend)/sizeof(weekend[0]); i++)
+        printf(" %d", weekend[i]);
+
+    return 0;
+}
