9ac97dc325149c83e2f44c9b61d577dd70d31568
makkusu-3d <maxdesign1990@gmail.com>
2018-09-30 11:34:39 +0100

    Create P09.c

---
+++ CH8/Programming Projects/P09.c
@@ -0,0 +1,136 @@
+/* Write a program that generates a "random walk" across
+ * a 10 x 10 array. The array will contain characters
+ * (all '.' initially). The program must randomly "walk"
+ * from element to element, always going up, down, left,
+ * or right by one element. The elements visited by the
+ * program will be labelled with the letter A
+ * through Z, in the order visited. Here's an example
+ * of the desired output:
+ *
+ * A . . . . . . . . .
+ * B C D . . . . . . .
+ * . F E . . . . . . .
+ * H G . . . . . . . .
+ * I . . . . . . . . .
+ * J . . . . . . . Z .
+ * K . . R S T U V Y .
+ * L M P Q . . . W X .
+ * . N O . . . . . . .
+ * . . . . . . . . . .
+ *
+ */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <time.h>
+ #include <stdbool.h>
+
+ #define N 10
+
+ char character;
+ int rand_num, i, j, k, m, ex;
+
+
+
+ int main(void) {
+
+     srand((unsigned) time(NULL));
+
+     unsigned char array[N][N];
+
+     for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            array[i][j] = '.';
+        }
+     }
+
+     i = rand() % 10;
+     j = rand() % 10;
+
+     array[i][j] = 'A';
+
+     for (character = 'B'; character <= 'Z'; character++) {
+
+        // Shows the array at each step for debugging
+        /*
+         for (k = 0; k < N; k++) {
+            printf("\n");
+            for (m = 0; m < N; m++) {
+                printf("%c", array[k][m]);
+            }
+        }
+        */
+
+        bool case_flag[4] = {0,0,0,0};
+
+        // printf("\n\n");
+        ex = 1;
+        while (ex) {
+            rand_num = rand() % 4;
+            switch(rand_num) {
+
+                case 3: // j + 1
+
+                    if (array[i][j + 1] == '.' && j + 1 != 10) {
+                        // printf("| %c: j + 1 |\n", character); // Shows step taken in console
+                        array[i][++j] = character;
+                        ex = 0;
+                    } else {
+                        case_flag[3] = 1;
+                        // printf("\nCase flag 3: %d\n", case_flag[3]);
+                    }
+                    break;
+
+                case 2: // j - 1
+
+                    if (array[i][j - 1] == '.' && j - 1 != -1){
+                        // printf("| %c: j - 1 |\n", character); // Shows step taken in console
+                        array[i][--j] = character;
+                        ex = 0;
+                    } else {
+                        case_flag[2] = 1;
+                        // printf("\nCase flag 2: %d\n", case_flag[2]);
+                    }
+                    break;
+
+                case 1: // i + 1
+
+                    if (array[i + 1][j] == '.' && i + 1 != 10) {
+                        // printf("| %c: i + 1  |\n", character); // Shows step taken in console
+                        array[++i][j] = character;
+                        ex = 0;
+                    } else {
+                        case_flag[1] = 1;
+                        // printf("\nCase flag 1: %d\n", case_flag[1]);
+                    }
+                    break;
+
+                case 0: // i - 1
+
+                    if (array[i - 1][j] == '.' && i - 1 != -1) {
+                        // printf("| %c: i - 1 |\n", character); // Shows step taken in console
+                        array[--i][j] = character;
+                        ex = 0;
+                    } else {
+                        case_flag[0] = 1;
+                        // printf("\nCase flag 0: %d\n", case_flag[0]);
+                    }
+                    break;
+            }
+            if (case_flag[0] == 1 && case_flag[1] == 1 && case_flag[2] == 1 && case_flag[3] == 1){
+                ex = 0;
+                character = 91;
+            }
+        }
+     }
+
+     for (i = 0; i < N; i++) {
+        printf("\n");
+        for (j = 0; j < N; j++) {
+            printf("%c", array[i][j]);
+        }
+     }
+     return 0;
+ }
+
+
