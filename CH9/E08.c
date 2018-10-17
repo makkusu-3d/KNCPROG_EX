cb9f4909ad77bdfe3b0c001ff25f4cce04b43bda
makkusu-3d <maxdesign1990@gmail.com>
2018-10-17 09:29:26 +0100

    Update E08.c

--- CH9/E08.c
+++ CH9/E08.c
@@ -3,9 +3,9 @@
  * function that returns nothing has one double parameter
  *
  * (a) void f(double x); --> Valid Prototype: Compiler knows
- *                           the function return nothing due
+ *                           the function returns nothing due
  *                           to the void and that the 
- *                           parameter take double.
+ *                           parameter takes type double.
  *                           
  * (b) void f(double);   --> Valid Prototype: For the same
  *                           as noted above. (You don't need
@@ -19,5 +19,7 @@
  *  
  * (d) f(double x);      --> Invalid prototype: There is no
  *                           return type for the function
- *                           prototype therefore invalid.
+ *                           prototype therefore invalid with C99.
+ *                           Valid in C89, but will get a return type
+ *                           of int.
  */
