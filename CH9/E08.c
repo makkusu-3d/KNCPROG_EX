85e8bbca9d41f0a0a74adb6a7f2f3a9cf962c3af
makkusu-3d <maxdesign1990@gmail.com>
2018-10-17 09:26:41 +0100

    Create E08.c

---
+++ CH9/E08.c
@@ -0,0 +1,23 @@
+/**
+ * Which of the following would be valid prototypes for
+ * function that returns nothing has one double parameter
+ *
+ * (a) void f(double x); --> Valid Prototype: Compiler knows
+ *                           the function return nothing due
+ *                           to the void and that the 
+ *                           parameter take double.
+ *                           
+ * (b) void f(double);   --> Valid Prototype: For the same
+ *                           as noted above. (You don't need
+ *                           to list the parameter names in
+ *                           the function prototype)
+ *
+ * (c) void f(x);        --> Invalid prototype: The compiler
+ *                           does not know what type the
+ *                           parameter is and will do
+ *                           default integer promotion. 
+ *  
+ * (d) f(double x);      --> Invalid prototype: There is no
+ *                           return type for the function
+ *                           prototype therefore invalid.
+ */
