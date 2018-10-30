282fe69238df6b0094804473781271ef264b06c6
makkusu-3d <maxdesign1990@gmail.com>
2018-10-30 06:17:23 +0100

    Create E03.c

---
+++ CH10/Exercises/E03.c
@@ -0,0 +1,21 @@
+/**
+ * Suppose that a program has only one function (main). How many
+ * different variables named i could this program contain?
+ *
+ * By creating block with { int i } you could create a indefinite
+ * amount of variables named i in the main function.
+ */
+
+ // For Example
+ #include <stdio.h>
+
+ int main(void)
+ {
+     int i, i;
+     {
+         int i
+     }
+     {
+         int i
+     }
+ }
