c9fd6c18f297767f5cd9798b910ed8dd2bd0c284
makkusu-3d <maxdesign1990@gmail.com>
2018-11-14 09:37:19 +0100

    Create E08.c

---
+++ CH12/Exercises/E08.c
@@ -0,0 +1,20 @@
+/**
+ * Rewrite the following function to use pointer arithmetic
+ * instead of array subscripting. (In other words, eliminate
+ * the variable i and all uses of the [] operator.) Make
+ * as few changes as possible.
+ *
+ * void store_zeros(int a[], int n)
+ * {
+ *    int i;
+ *
+ *    for (i = 0; i < n; i++)
+ *       a[i] = 0;
+ * }
+ */
+
+void store_zeros(int *p, int n)
+{
+    for (p = a; p < a + n; p++)
+        p* = 0;
+}
