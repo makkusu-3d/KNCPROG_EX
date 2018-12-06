605e9e9bc47908dcd4cfbc1f3909e4a825f2e022
makkusu-3d <maxdesign1990@gmail.com>
2018-12-06 23:33:12 +0100

    Create E07.c

---
+++ CH13/Exercises/E07.c
@@ -0,0 +1,15 @@
+/**
+ * Suppose that str is an array of characters. Which one of
+ * the following statements is not equivalent to the other
+ * three?
+ *
+ * (a) *str = 0;
+ * (b) str[0] = '\0';
+ * (c) strcpy(str, "");
+ * (d) strcat(str, "");
+ *
+ * (d) Is the non equivalent one since is concatenates
+ *     the null character to the end of a string
+ */
+
+
