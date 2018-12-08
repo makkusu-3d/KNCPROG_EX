e0e7364943c40bff98404137cc01d94ccb4b5579
makkusu-3d <maxdesign1990@gmail.com>
2018-12-08 18:18:03 +0100

    Create E08.c

---
+++ CH13/Exercises/E08.c
@@ -0,0 +1,17 @@
+/**
+ * What will be the value of the string str after the
+ * following statements have been executed?
+ *
+ * strcpy(str, "tire-bouchon");
+ * The value: char str[] = "tire-bouchon";
+ *
+ * strcpy(&str[4], "d-or-wi");
+ * Copies the string at starting at the address
+ * given in the first argument.
+ * The value: char str[] = "xxxxd-or-wi";
+ *
+ * strcat(str, "red?");
+ * concatenates the input string into the second
+ * argument with it starting at the null ('\0') of
+ * the end of the string for argument 1.
+ */
