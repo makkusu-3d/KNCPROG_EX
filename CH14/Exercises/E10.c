0ecf364f354b9eab24edda74deb253dc35e40ee5
makkusu-3d <maxdesign1990@gmail.com>
2019-01-15 21:22:07 +0100

    Create E10.c

---
+++ CH14/Exercises/E10.c
@@ -0,0 +1,8 @@
+/**
+ * Function can often--but not always--be written as
+ * parametrized macros. Discuss what characteristics
+ * of a function would make it unsuitable as a macro.
+ *
+ * If the function implements recursion, requires debugging
+ * and requires smaller binary code.
+ */
