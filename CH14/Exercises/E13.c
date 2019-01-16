e50fffb333e10a4414d3a74a34b36ad10bb7d944
makkusu-3d <maxdesign1990@gmail.com>
2019-01-16 20:44:16 +0100

    Create E13.c

---
+++ CH14/Exercises/E13.c
@@ -0,0 +1,52 @@
+/**
+ * (a) Show what the following program will look like after
+ * preprocessing. You may ignore any lines added to the
+ * program as a result of including the <stdio.h> header.
+ *
+ * #include <stdio.h>
+ *
+ * #define N 100
+ *
+ * void f(void);
+ *
+ * int main(void)
+ * {
+ *   f();
+ * #ifdef N
+ * #undef N
+ * #endif
+ *   return 0;
+ * }
+ *
+ * void f(void)
+ * {
+ * #if defined(N)
+ *   printf("N is %d\n", N);
+ * #else
+ *   printf("N is undefined\n");
+ * #endif
+ * }
+ *
+ * (b) What will be the output of this program?
+ */
+
+ /**
+  * (a) After preprocessing
+  *
+  * void f(void)
+  *
+  * int main(void)
+  * {
+  *   f();
+  *   return 0;
+  * }
+  *
+  * void f(void)
+  * {
+  *    printf("100 is %d\n", 100);
+  * }
+  *
+  * (b) Output of this program
+  *
+  *  100 is 100
+  */
