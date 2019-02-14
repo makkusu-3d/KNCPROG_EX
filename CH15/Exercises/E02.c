a5d21de84e23869f5b3fb1d512e9423a4b8ef255
makkusu-3d <maxdesign1990@gmail.com>
2019-02-14 13:22:01 +0100

    Create E02.c

---
+++ CH15/Exercises/E02.c
@@ -0,0 +1,14 @@
+/**
+ * Which of the following should not be put in a header
+ * file? Why not?
+ *
+ * (a) Function Prototypes
+ * (b) Function Definitions
+ * (c) Macro Definitions
+ * (d) Type definitions
+ *
+ * You should not put (b) in a header file.
+ * This is because you do not want multiple defined
+ * versions of that function after compiling and linking
+ * this will cause a error.
+ */
