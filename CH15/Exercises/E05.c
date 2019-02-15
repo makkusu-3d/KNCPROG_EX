5230ccc140e8ff9b49a639fdb31d3dcea64bb1d3
makkusu-3d <maxdesign1990@gmail.com>
2019-02-15 13:26:06 +0100

    Create E05.c

---
+++ CH15/Exercises/E05.c
@@ -0,0 +1,22 @@
+/**
+ * Suppose that a program consists of three source files
+ * -main.c, f1.c and f2.c - plus two header files, f1.h,
+ * but only f1.c and f2.c include f2.h. Write a makefile
+ * for this program, assuming that the compiler is gcc
+ * and that the executable file is to be named demo.
+ *
+ * Create a makefile
+ *
+ * demo: main.o f1.o f2.o
+ *       gcc -o demo main.o f1.o f2.o
+ *
+ * main.o: main.c f1.h
+ *       gcc -c main.c
+ *
+ * f1.o: f1.c f1.h f2.h
+ *       gcc -c f1.c
+ *
+ * f2.o: f2.c f1.h f2.h
+ *       gcc -c f2.c
+ */
+
