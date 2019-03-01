eca824a073f7648b2e66f0c990891f63fd1897bb
makkusu-3d <maxdesign1990@gmail.com>
2019-03-01 14:38:25 +0100

    Update P04.c

--- CH16/Programming Projects/P04.c
+++ CH16/Programming Projects/P04.c
@@ -1,36 +1,3 @@
-/* The following table shows the daily flights from one city to another:
- *
- * Departure time   Arrival time_base
- *  8:00 a.m.           10:16 a.m.  480  min
- *                                              531.5  min -> 532
- *  9:43 a.m.           11:52 a.m.  583  min
- *                                              631    min
- * 11:19 a.m.            1:31 p.m.  679  min
- *                                              723    min
- * 12:47 p.m.            3:00 p.m.  767  min
- *                                              803.5  min -> 804
- *  2:00 p.m.            4:08 p.m.  840  min
- *                                              892.5  min -> 893
- *  3:45 p.m.            5:55 p.m.  945  min
- *                                              1042.5 min -> 1043
- *  7:00 p.m.            9:20 p.m.  1140 min
- *                                              1222.5 min -> 1223
- *  9:45 p.m.           11:58 p.m.  1305 min
- *
- * The departure times lies in certain ranges of minutes
- *
- * 532 min |<--------|-------->| 631 min
- *               9:43 a.m
- *
- * Write a program that asks user to enter a time (expressed in hours and
- * minutes, using the 24-hour clock). The program then displays the
- * departure and arrival times for the flight whose departure time is
- * closest to that entered by the user:
- *
- * Enter a 24-hour time: 13:15
- * Closest departure time is 12:47 p.m., arriving at 3:00 p.m.
- */
-
 #include <stdio.h>
 #define N 8
 
