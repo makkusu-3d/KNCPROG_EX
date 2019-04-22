6c934a17cb54e109144272cb7bb7351639449845
makkusu-3d <maxdesign1990@gmail.com>
2019-04-22 21:28:22 +0200

    Update E16.c

--- CH17/Exercises/E16.c
+++ CH17/Exercises/E16.c
@@ -1,1 +1,14 @@
+/**
+ * Write the following function. The call sum(g,i,j)
+ * should return g(i) + ... + g(j).
+ * int sum(int (*f)(int), int start, int end);
+ */
 
+int sum(int (*f)(int), int start, int end)
+{
+    int i, sum = 0;
+    for(i = start; i <= end; i++) {
+        sum += f(i);
+    }
+    return sum;
+}
