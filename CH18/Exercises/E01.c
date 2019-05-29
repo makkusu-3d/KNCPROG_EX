931e3d26239f023b7d88e06dbc9e04510b2ccb2d
makkusu-3d <maxdesign1990@gmail.com>
2019-05-29 10:09:14 +0200

    Create E01.c

---
+++ CH18/Exercises/E01.c
@@ -0,0 +1,18 @@
+/**
+ * For each of the following declarations, identify the
+ * storage class, type qualifiers, type specifiers,
+ * and initializers
+ *
+ * (a)  static          char**          lookup(int level)
+ *      storage class   type specifier  declarator
+ *
+ * (b)  volatile        unsigned long   io_flags;
+ *      type qualifier  type specifier  declarator
+ *
+ * (c)  extern          char            *file_name[MAX_FILES], path[];
+ *      storage class   type specifier  declarator
+ *
+ * (d)  static          const           char            token_buf[] = "";
+ *      storage class   type qualifier  type specifier  declarator  initializer
+ */
+
