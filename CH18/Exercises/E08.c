daa12ebd3bf86e860369432526094194a122bcdc
makkusu-3d <maxdesign1990@gmail.com>
2019-05-31 11:04:43 +0200

    Create E08.c

---
+++ CH18/Exercises/E08.c
@@ -0,0 +1,23 @@
+/**
+ * Write a complete description of the type of x as specified
+ * by each of the following declarations.
+ *
+ * (a) char  (*x[10])(int);
+ *     ANS:  An array of 10 pointers to functions with
+ *           a argument of int that return a char type
+ *
+ * (b) int   (*x(int))[5];
+ *     ANS:  A function with a argument of int that
+ *           return a pointer to an array of 5 integers
+ *
+ * (c) float (*(*x(void))(int);
+ *     ANS:  A function with no argument that returns a
+ *           pointer to pointer to a function with argument
+ *           of int that returns float
+ *
+ * (d) void  (*x(int, void (*y)(int)))(int);
+ *     ANS:  function with arguments of int and function
+ *           of argument of int that returns void pointer
+ *           that return pointer to function that as has
+ *           argument of int and returns void
+ */
