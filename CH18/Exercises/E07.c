696a22349e0ff58b24174dee2c187b287024ff09
makkusu-3d <maxdesign1990@gmail.com>
2019-05-31 10:16:30 +0200

    Create E07.c

---
+++ CH18/Exercises/E07.c
@@ -0,0 +1,17 @@
+/**
+ * Suppose that we declare x to be const object. Which one of the
+ * following statements x is false?
+ *
+ * (a) If x is of type int, it can be used as the value of a case
+ *     label in a switch statement.
+ *     ANS: True
+ *
+ * (b) The compiler will check that no assignment is made to x.
+ *     ANS: False
+ *
+ * (c) x is subject to the same scope rules as variables.
+ *     ANS: True
+ *
+ * (d) x can be of any type. (data type?)
+ *     ANS: True
+ */
