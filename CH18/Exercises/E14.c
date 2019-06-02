6e187219d96040f4ffb18692741a9bd5ca2bcc7f
makkusu-3d <maxdesign1990@gmail.com>
2019-06-02 10:30:27 +0200

    Create E14.c

---
+++ CH18/Exercises/E14.c
@@ -0,0 +1,11 @@
+/**
+ * Which kind of variables cannot be initialized?
+ *
+ * (a) Array variables
+ * (b) Enumeration variables
+ * (c) Structure variables
+ * (d) Union variables
+ * (e) None of the above
+ *
+ * ANS: (e)
+ */
