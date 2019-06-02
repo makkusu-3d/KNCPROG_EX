ba33b846c31a8e19b38bd9e24166976e494f74b3
makkusu-3d <maxdesign1990@gmail.com>
2019-06-02 10:40:34 +0200

    Create E15.c

---
+++ CH18/Exercises/E15.c
@@ -0,0 +1,15 @@
+/**
+ * Which property of a variable determines whether or not it has
+ * a default initial value?
+ *
+ * (a) Storage duration
+ * (b) Scope
+ * (c) Linkage
+ * (d) Type
+ *
+ * ANS: (a)
+ *
+ * "The first initialization of that variable is the zero-initialization
+ *  of objects with static storage duration that happens before any
+ *  other initialization takes place"
+ */
